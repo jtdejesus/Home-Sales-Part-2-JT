297a03731eebde43c042a0c24f9083915708100d
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-06-23 19:09:48 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int salespersonD= 1550;
+    int salespersonE= 89000;
+    int salespersonF= 0;;
+    Console.WriteLine("Grand Total: " + (salespersonD + salespersonE + salespersonF));
+    Console.WriteLine ("Highest Sale: D");
+  }
+}
